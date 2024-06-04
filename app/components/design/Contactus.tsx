7a695fdca4d6444d0dd39586ef576b5bdf0e58ba
sakthipriya2904 <sakthipriya.c.2021.cse@ritchennai.edu.in>
2024-06-04 16:21:17 +0530

    contactus2

--- app/components/design/Contactus.tsx
+++ app/components/design/Contactus.tsx
@@ -2,6 +2,7 @@
 import { MessageCircle } from 'lucide-react'
 import { useState } from 'react'
 import React from 'react'
+import Button from '../Button';
 
 function Contactus() {
 
@@ -12,25 +13,22 @@ function Contactus() {
   };
   return (
     <>
-    <button 
-      onClick={toggleModal} 
-      className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      type="button"
-    >
-      Toggle modal
+    <button className = "bg- rounded-lg" onClick={toggleModal} >
+      
+      <MessageCircle />
     </button>
 
     {isOpen && (
       <div id="authentication-modal z-1"  aria-hidden="true" className="fixed inset-0 z-50 flex justify-center items-center w-full h-full overflow-y-auto overflow-x-hidden">
-        <div className="relative p-4 w-full max-w-md max-h-full">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Sign in to our platform
+        <div className="relative  w-full max-w-md max-h-full">
+          <div className="relative bg-n-6 rounded-lg  ">
+            <div className="flex items-center justify-between mt-2 md:p-5 rounded-md">
+              <h3 className="text-xl font-semibold text-white">
+                Contact us
               </h3>
               <button 
                 type="button" 
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" 
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" 
                 onClick={toggleModal}
               >
                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -40,51 +38,31 @@ function Contactus() {
               </button>
             </div>
             <div className="p-4 md:p-5">
-              <form className="space-y-4" action="#">
+              <form className="space-y-4 p-2" action="#">
                 <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text--900 dark:text-white">Email</label>
                   <input 
                     type="email" 
                     name="email" 
                     id="email" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
                     placeholder="name@company.com" 
                     required 
                   />
                 </div>
                 <div>
-                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                  <input 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    placeholder="••••••••" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
-                    required 
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Message</label>
+                  <textarea 
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"
                   />
                 </div>
-                <div className="flex justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input 
-                        id="remember" 
-                        type="checkbox" 
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" 
-                        required 
-                      />
-                    </div>
-                    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                  </div>
-                  <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                </div>
+                <div className='gap-2'>
                 <button 
                   type="submit" 
                   className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Login to your account
+                  Submit
                 </button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                 </div>
               </form>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,7 @@ export default function Page() {
   return (
     <div>
       <Header/>
-      <Hero/>
       <Benefits/>
-      <Services/>
       <Roadmap/>
       <Footer/>
       <Contactus/>
