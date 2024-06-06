14846ba3bb9cc973edbd2f1e61eae637ffd08a33
Hackprakas <vijayatrprakash@gmail.com>
2024-06-06 13:15:33 +0530

    testimonial changes

--- app/components/testimonials.tsx
+++ app/components/testimonials.tsx
@@ -4,43 +4,43 @@ import Button from "../components/Button";
 import Tagline from "../components/Tagline";
 import { Gradient } from "../components/design/Roadmap";
 import Image from "next/image";
+import { roadmap } from "../constants";
+import { Quote } from 'lucide-react';
+
 
 
 function Testimonials() {
   const cards = [
-    { id: 1, image: grid, text: "Kids have a beautiful mind. What they see and hear can inspire them I a very different way. Every child can imagine things with uniqueness. It is very easy to inspire and motivate them to find their dreams through Vaayusastra's teaching model. Let it be practical model, ancient history, fun activities and VAAYUSASTRA CAN DO IT."},
-    { id: 2, image: grid, text: "When I was little I so wished subjects were as interesting as watching a movie, the fact that Vaayusastra uses arts to teach science is almost like a wish come true to people like me. This method will bring both the scientist and artist in a kid. I am so happy that kids this generation are given an opportunity to experience science " },
-    { id: 3, image: grid, text: "Aviation has a special place for itself in the emerging courses. Our Vaayusastra gives such a study in a very beautiful form in the form of drama. This quality educational system is in the form of a play by the best trainers to make it easy for children to understand. They have been doing this endeavor for the last 2 years with excellent teaching skills" },
+    { id: 1, image: grid, text: "Kids have a beautiful mind. What they see and hear can inspire them I a very different way. Every child can imagine things with uniqueness. It is very easy to inspire and motivate them to find their dreams through Vaayusastra's teaching model. Let it be practical model, ancient history, fun activities and VAAYUSASTRA CAN DO IT.", name:"Abhi Dharshan",designation:"Pilot/Aeronautical engineer Founding member",company:"Vaayusastra Aerospace"},
+    { id: 2, image: grid, text: "When I was little I so wished subjects were as interesting as watching a movie, the fact that Vaayusastra uses arts to teach science is almost like a wish come true to people like me. This method will bring both the scientist and artist in a kid. I am so happy that kids this generation are given an opportunity to experience science ",name:"Gayathri MS",designation:"  Founding Member",company:"Aerospace (PDM UniversityMilan, Italy)" },
+    { id: 3, image: grid, text: "Aviation has a special place for itself in the emerging courses. Our Vaayusastra gives such a study in a very beautiful form in the form of drama. This quality educational system is in the form of a play by the best trainers to make it easy for children to understand. They have been doing this endeavor for the last 2 years with excellent teaching skills", name:"Nivash",designation:"Customer Agent",company:"AIAPSL Chennai International Airport"},
   ];
 
   return (
-    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+    <div className="p-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+      
       {cards.map((card) => (
-        <div key={card.id} className="rounded-lg overflow-hidden">
-          <div className="relative border border-n-6 rounded-lg">
-            <Image
-              className="w-full h-full object-cover"
-              src={card.image}
-              alt={`Card ${card.id}`}
-            />
+        
+        <div key={card.id} className="rounded-lg ">
+         
+          <div className="relative border border-n-6 rounded-lg mb-14">
+            <div className="flex justify-center">
+
+          <Image className="w-32 h-32 z-5 p-1 rounded-full translate-y-[-60px]  bg-blend-normal ring-2 ring-gray-300 dark:ring-gray-500" src={card.image} alt="Bordered avatar"/>
+            </div>
+            <div className="flex flex-col items-center">
+
+            <div className="mt-[-30px] font-extrabold text-center">{card.name}</div>
+            <div className="text-center">{card.designation}</div>
+            <div className="mb-10 text-center">{card.company}</div>
+
+            <div className="bg-purple-700 h-10 w-10 rounded-full flex justify-center items-center mb-8">
+            <Quote />
+            </div>
             <div className="p-4">
-              <div className="flex items-center justify-between mb-4">
-                <div className="flex items-center px-2 py-1 bg-white rounded-full">
-                  {/* <Image
-                    className="mr-1"
-                    src={check2}
-                    width={12}
-                    height={12}
-                    alt="Check"
-                  /> */}
-                </div>
-              </div>
-              <div>
-                <p className="text-white text-xs">{card.text}</p>
-                <Tagline className="text-white text-xs">TESTIMONIAL</Tagline>
-              </div>
-              <h4 className="h4 mb-2 text-white"></h4>
-              <p className="body-2 text-n-4 text-white">Founding member Vaayusastra Aerospace</p>
+             {card.text}
+
+            </div>
             </div>
           </div>
         </div>
