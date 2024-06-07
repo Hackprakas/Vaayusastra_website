99d93d6a030b945e8b0052853d596812a700f6fd
sakthipriya2904 <sakthipriya.c.2021.cse@ritchennai.edu.in>
2024-06-07 15:10:58 +0530

    testimonial --details

--- app/components/testimonials.tsx
+++ app/components/testimonials.tsx
@@ -6,24 +6,24 @@ import { Gradient } from "../components/design/Roadmap";
 import Image from "next/image";
 import { roadmap } from "../constants";
 import { Quote } from 'lucide-react';
+import { testmon } from "../constants";
+import Section from "./Section";
 
 
 
 function Testimonials() {
-  const cards = [
-    { id: 1, image: grid, text: "Kids have a beautiful mind. What they see and hear can inspire them I a very different way. Every child can imagine things with uniqueness. It is very easy to inspire and motivate them to find their dreams through Vaayusastra's teaching model. Let it be practical model, ancient history, fun activities and VAAYUSASTRA CAN DO IT.", name:"Abhi Dharshan",designation:"Pilot/Aeronautical engineer Founding member",company:"Vaayusastra Aerospace"},
-    { id: 2, image: grid, text: "When I was little I so wished subjects were as interesting as watching a movie, the fact that Vaayusastra uses arts to teach science is almost like a wish come true to people like me. This method will bring both the scientist and artist in a kid. I am so happy that kids this generation are given an opportunity to experience science ",name:"Gayathri MS",designation:"  Founding Member",company:"Aerospace (PDM UniversityMilan, Italy)" },
-    { id: 3, image: grid, text: "Aviation has a special place for itself in the emerging courses. Our Vaayusastra gives such a study in a very beautiful form in the form of drama. This quality educational system is in the form of a play by the best trainers to make it easy for children to understand. They have been doing this endeavor for the last 2 years with excellent teaching skills", name:"Nivash",designation:"Customer Agent",company:"AIAPSL Chennai International Airport"},
-  ];
+  
 
   return (<>
+  <Section>
+
   <div className="flex justify-center mb-7">
 
       <h4 className="h4">Testimonials</h4>
   </div>
     <div className="p-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 ">
       
-      {cards.map((card) => (
+      {testmon.map((card) => (
         
         <div key={card.id} className="rounded-lg ">
          
@@ -50,12 +50,13 @@ function Testimonials() {
         </div>
       ))}
       <Gradient />
-      <div className="flex justify-center w-screen mt-6 md:mt-8 xl:mt-10">
+      <div className="flex justify-center w-full mt-6 md:mt-8 xl:mt-10">
         <Button white href="#">
           Go Back
         </Button>
       </div>
     </div>
+  </Section>
     </>
   );
 }
