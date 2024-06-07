e67780aba084536165924cfebba6bab00fffbd89
ram05kishor12 <ramkishore29861@gmail.com>
2024-06-07 11:08:05 +0530

    calculator page  -- 3 functions added

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Image from 'next/image';
 import React, { useState, ChangeEvent } from 'react';
+import { grid } from '../components/assets';
 
 const planetGravities: { [key: string]: number } = {
     Mercury: 0.38,
@@ -13,31 +15,87 @@ const planetGravities: { [key: string]: number } = {
     Pluto: 0.06
 };
 
-const WeightConverter: React.FC = () => {
-    const [earthWeight, setEarthWeight] = useState<string>('');
+const planetOrbitalPeriods: { [key: string]: number } = {
+    Mercury: 0.24,
+    Venus: 0.62,
+    Earth: 1,
+    Mars: 1.88,
+    Jupiter: 11.86,
+    Saturn: 29.46,
+    Uranus: 84.01,
+    Neptune: 164.79,
+    Pluto: 248.59
+};
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+const Calculator: React.FC = () => {
+    const [earthWeight, setEarthWeight] = useState<string>('');
+    const [earthAge, setEarthAge] = useState<string>('');
+    const [celsius, setCelsius] = useState<string>('');
+    const [fahrenheit, setFahrenheit] = useState<string>('');
+
+    const handleWeightInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setEarthWeight(e.target.value);
+    };
+
+    const handleAgeInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEarthAge(e.target.value);
+    };
+
+    const handleCelsiusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setCelsius(value);
+        if (value !== '' && !isNaN(parseFloat(value))) {
+            setFahrenheit(((parseFloat(value) * 9/5) + 32).toFixed(2));
+        } else {
+            setFahrenheit('');
+        }
+    };
+
+    const handleFahrenheitChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setFahrenheit(value);
+        if (value !== '' && !isNaN(parseFloat(value))) {
+            setCelsius(((parseFloat(value) - 32) * 5/9).toFixed(2));
+        } else {
+            setCelsius('');
+        }
     };
 
     const calculateWeight = (weight: number, gravity: number): string => {
         return (weight * gravity).toFixed(2);
     };
 
+    const calculateAge = (age: number, orbitalPeriod: number): string => {
+        return (age / orbitalPeriod).toFixed(2);
+    };
+
     return (
-        <div className="flex justify-center items-center h-screen p-8">
-            <div className="flex flex-col items-center bg-n-7 p-8 rounded-[2.5rem] lg:min-w-[300px] lg:max-w-[800px] h-full overflow-auto max-h-screen space-y-4 shadow-2xl">
-                <h4 className="h4 mb-4 font-extrabold text-center">Weight on Other Planets</h4>
-                <div className="flex flex-col space-y-2 w-full">
-                    <div className="flex items-center justify-center w-full mb-6">
-                        <label htmlFor="weight" className="block mb-2 text-sm font-medium text-white space-y-4">Enter your weight on Earth (kg)</label>
+        <div className='bg-n-8 flex flex-col justify-center items-center p-4'>
+            <div className="relative p-8 bg-n-7 rounded-[2.4375rem] overflow-hidden max-w-lg w-full xl:p-15 mb-8 mx-auto">
+                <div className="absolute top-0 left-0 max-w-full">
+                    <Image
+                        className="w-full"
+                        src={grid}
+                        width={550}
+                        height={550}
+                        alt="Grid"
+                    />
+                </div>
+                <div className="z-1">
+                    <div className="mb-8 md:mb-20">
+                        <h4 className="h4 mb-4 font-extrabold text-center">Weight on Other Planets</h4>
+                    </div>
+                    <div className="flex items-center justify-center w-full mb-6 gap-2">
+                        <label htmlFor="weight" className="block mb-2 text-sm font-medium text-white">
+                            Enter your weight on Earth (kg)
+                        </label>
                         <input 
                             type="number" 
                             id="weight" 
                             name="weight" 
                             value={earthWeight} 
-                            onChange={handleInputChange}
-                            className="bg-gray-800 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            onChange={handleWeightInputChange}
+                            className="bg-gray-800 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                             placeholder="Your weight on Earth"
                         />
                     </div>
@@ -45,9 +103,95 @@ const WeightConverter: React.FC = () => {
                         {Object.keys(planetGravities).map((planet) => (
                             <div key={planet} className="flex justify-between">
                                 <span>{planet}</span>
-                                <span>{earthWeight && !isNaN(parseFloat(earthWeight)) ? calculateWeight(parseFloat(earthWeight), planetGravities[planet]) + ' kg' : '-'}</span>
+                                <span>
+                                    {earthWeight && !isNaN(parseFloat(earthWeight)) ? calculateWeight(parseFloat(earthWeight), planetGravities[planet]) + ' kg' : '-'}
+                                </span>
                             </div>
                         ))}
+                    </div>
+                </div>
+            </div>
+            <div className="relative p-8 bg-n-7 rounded-[2.4375rem] overflow-hidden max-w-lg w-full xl:p-15 mb-8 mx-auto">
+                <div className="absolute top-0 left-0 max-w-full">
+                    <Image
+                        className="w-full"
+                        src={grid}
+                        width={550}
+                        height={550}
+                        alt="Grid"
+                        />
+                </div>
+                <div className="z-1">
+                    <div className="mb-8 md:mb-20">
+                        <h4 className="h4 mb-4 font-extrabold text-center">Age on Other Planets</h4>
+                    </div>
+                    <div className="flex items-center justify-center w-full mb-6 gap-2">
+                        <label htmlFor="age" className="block mb-2 text-sm font-medium text-white">
+                            Enter your age on Earth (years)
+                        </label>
+                        <input 
+                            type="number" 
+                            id="age" 
+                            name="age" 
+                            value={earthAge} 
+                            onChange={handleAgeInputChange}
+                            className="bg-gray-800 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            placeholder="Your age on Earth"
+                            />
+                    </div>
+                    <div className="flex flex-col space-y-2 text-zinc-500 text-sm">
+                        {Object.keys(planetOrbitalPeriods).map((planet) => (
+                            <div key={planet} className="flex justify-between">
+                                <span>{planet}</span>
+                                <span>
+                                    {earthAge && !isNaN(parseFloat(earthAge)) ? calculateAge(parseFloat(earthAge), planetOrbitalPeriods[planet]) + ' years' : '-'}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="relative p-8 bg-n-7 rounded-[2.4375rem] overflow-hidden max-w-lg w-full xl:p-15 mx-auto">
+                <div className="absolute top-0 left-0 max-w-full">
+                    <Image
+                        className="w-full"
+                        src={grid}
+                        width={550}
+                        height={550}
+                        alt="Grid"
+                    />
+                </div>
+                <div className="z-1">
+                    <div className="mb-8 md:mb-20">
+                        <h4 className="h4 mb-4 font-extrabold text-center">Temperature Converter</h4>
+                    </div>
+                    <div className="flex items-center justify-center w-full mb-6 gap-4">
+                        <label htmlFor="celsius" className="block mb-2 text-sm font-medium text-white">
+                            Enter temperature in Celsius (°C)
+                        </label>
+                        <input 
+                            type="number" 
+                            id="celsius" 
+                            name="celsius" 
+                            value={celsius} 
+                            onChange={handleCelsiusChange}
+                            className="bg-gray-800 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 z-1"
+                            placeholder="Celsius"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center w-full mb-6 gap-4">
+                        <label htmlFor="fahrenheit" className="block mb-2 text-sm font-medium text-white">
+                            Enter temperature in Fahrenheit (°F)
+                        </label>
+                        <input 
+                            type="number" 
+                            id="fahrenheit" 
+                            name="fahrenheit" 
+                            value={fahrenheit} 
+                            onChange={handleFahrenheitChange}
+                            className="bg-gray-800 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 z-1"
+                            placeholder="Fahrenheit"
+                        />
                     </div>
                 </div>
             </div>
@@ -55,4 +199,4 @@ const WeightConverter: React.FC = () => {
     );
 }
 
-export default WeightConverter;
+export default Calculator;
