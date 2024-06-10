ad152bca48b04b21c1f2a75143f0f89535145d85
sakthipriya2904 <sakthipriya.c.2021.cse@ritchennai.edu.in>
2024-06-10 12:25:21 +0530

    papers2

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { benefits } from "../constants";
+import { benefits, navigation } from "../constants";
 import Heading from "../components/Heading";
 import Section from "../components/Section";
 import Arrow from "../components/assets/svg/Arrow";
@@ -9,25 +9,33 @@ import Image from "next/image";
 import Link from "next/link";
 import { Papers } from "../constants";
 import {grid}from "../components/assets"
+import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 
 const Course: React.FC = () => {
   return (
+    
+    <>
+    <div>
+    <Navbar data={navigation} />
     <Section id="features">
+      <div className="mt-20"> 
+
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl font-bold"
           title="Papers & Articles"
-        />
+          />
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 mb-10">
           {Papers.map((item) => (
-            <div
+              <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
-              key={item.id}
-            >
+                  backgroundImage: `url(${item.backgroundUrl})`,
+                  }}
+                  key={item.id}
+                  >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] ">
                 
                 <h5 className="h5 mb-5">{item.title}</h5>
@@ -38,7 +46,7 @@ const Course: React.FC = () => {
                     width={48}
                     height={48}
                     alt={item.title}
-                  />
+                    />
                   <a href={item.url}>
                     <div className="flex">
 
@@ -57,17 +65,17 @@ const Course: React.FC = () => {
               <div
                 className="absolute inset-0.5 bg-n-8"
                 style={{ clipPath: "url(#benefits)" }}
-              >
+                >
                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
                   {item.imageUrl && (
-                    <Image
+                      <Image
                       src={item.imageUrl}
                       width={380}
                       height={362}
                       alt={item.title}
                       className="w-full h-full object-cover"
-                    />
-                  )}
+                      />
+                      )}
                 </div>
               </div>
 
@@ -75,8 +83,12 @@ const Course: React.FC = () => {
             </div>
           ))}
         </div>
+        </div>
       </div>
     </Section>
+    <Footer/>
+                      </div> 
+    </>
   );
 };
 
