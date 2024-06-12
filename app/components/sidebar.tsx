1eaf55483cd279bc00a76710b7e9aae7f40c588f
SriRanjaniiyyappan <sriranjanisri2103@gmail.com>
2024-06-12 15:30:30 +0530

    page update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -82,18 +82,7 @@ export default function Sidebar() {
         </div>
       </aside>
 
-      <div className="p-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4 w-full">
-        {Array(4).fill(0).map((_, index) => (
-          <div key={index} className="rounded-lg">
-            <div className="relative border border-n-6 rounded-lg mb-14 px-2">
-              <div className="flex flex-col items-center">
-                <div className="p-6 font-extrabold text-center">Card Title</div>
-                <div className="mb-10 text-center p-4 text-n-4">Card Content</div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      
     </div>
   );
 }
