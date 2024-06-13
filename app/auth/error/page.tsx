8428c32acb6a385e9af730a0a7d3ef6b9fac7359
Hackprakas <vijayatrprakash@gmail.com>
2024-06-13 15:22:48 +0530

    suspense error

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-export default function page() {
+ function Page() {
   const searchParams = useSearchParams()
   const message=searchParams.get('error')
   return (
-    <Suspense>
+    
 
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
@@ -15,6 +15,13 @@ export default function page() {
         <p className="text-lg mt-4">Please try again</p>
       </div>
     </div>
+   
+  )
+}
+function Error() {
+  return (
+    <Suspense>
+      <Page />
     </Suspense>
   )
 }
