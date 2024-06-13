cf5f49b760c37e4c7f479ff7542c0f191b7cd54f
Hackprakas <vijayatrprakash@gmail.com>
2024-06-13 15:14:59 +0530

    suspense change

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 import React from 'react'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function page() {
   const searchParams = useSearchParams()
   const message=searchParams.get('error')
   return (
+    <Suspense>
+
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-red-500">{message}</h1>
         <p className="text-lg mt-4">Please try again</p>
       </div>
     </div>
+    </Suspense>
   )
 }
