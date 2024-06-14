823c7461cae1a44a0a2c108a619d7ab3f9e4afd3
ram05kishor12 <ramkishore29861@gmail.com>
2024-06-14 11:13:21 +0530

    packages ~ added / custom cursor fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         
          <Authprovider >
           
-            {/* <CustomCursor /> */}
+            <CustomCursor />
             {children}
          </Authprovider>
          
