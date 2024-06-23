b2334fc14f56a67d69f7748549cb09d116166b88
Hackprakas <vijayatrprakash@gmail.com>
2024-06-23 09:58:33 +0530

    production error fix

--- app/dummy/page.tsx
+++ app/dummy/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 import React from 'react'
-import { getsession } from '@/actions/route2'
-import { getlist } from '@/actions/route5'
+
 
 export default function page() {
   return (<>
     <div>page</div>
-    <button onClick={()=>getlist()}>hi </button>
+    <button >hi </button>
   </>
   )
 }
