0a4cbba219162a1913c9f9ccb3dc35c5a9c1811e
ram05kishor12 <ramkishore29861@gmail.com>
2024-06-28 10:51:25 +0530

    final push ~all working fine

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
-import { LogOut, Users, UserPlus, CloudUpload, FileUp, UserRoundPlus, PieChart } from 'lucide-react';
+import { LogOut, Users, UserPlus, CloudUpload, FileUp, UserRoundPlus, PieChart, SquarePen } from 'lucide-react';
 import vslogo from "../../public/VS Logo New 1_edited_edited.webp";
 import { signOut } from 'next-auth/react';
 import Link from 'next/link';
@@ -53,7 +53,13 @@ function Sidebar() {
               <li>
               <Link href="/admin/table" className="flex items-center py-3 px-4 hover:bg-purple-700 rounded">
                 <ReceiptIndianRupee className="mr-2" />
-                Transactions
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/update" className="flex items-center py-3 px-4 hover:bg-purple-700 rounded">
+                <SquarePen className="mr-2" />
+                Update
               </Link>
             </li>
           </ul>
